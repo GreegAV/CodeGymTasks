788a98e9bebc41d295a4526e305d1e3f25f523e3
Андрей Григорьев <greegav@gmail.com>
2019-01-09 18:39:01 +0200

    Level 3

--- 1.JavaSyntax/src/com/codegym/task/task03/task0306/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task03/task0306/Solution.java
@@ -16,6 +16,7 @@ Requirements:
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println((2 * 3) + 4 * 5 + (6 * 7));
+//        System.out.println((2 * 3) + 4 * 5 + (6 * 7));
+        System.out.println(2 * (3 + 4 * (5 + 6 * 7)));
     }
 }
