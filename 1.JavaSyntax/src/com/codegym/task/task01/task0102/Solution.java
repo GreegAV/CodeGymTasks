3f83c1d37223843fd375336778ef1ff8dc344ef8
Андрей Григорьев <greegav@gmail.com>
2019-01-09 16:57:27 +0200

    Level 0

---
+++ 1.JavaSyntax/src/com/codegym/task/task01/task0102/Solution.java
@@ -0,0 +1,31 @@
+package com.codegym.task.task01.task0102;
+
+/* 
+Написать программу, выводящую на экран надпись «Элли самая красивая» 5 раз. Каждый раз – с новой строки.
+
+Пример вывода на экран:
+Элли самая красивая
+Элли самая красивая
+Элли самая красивая
+Элли самая красивая
+Элли самая красивая
+
+
+Требования:
+1. Программа должна выводить текст.
+2. Текст должен начинаться с «Элли».
+3. Текст должен заканчиваться на «красивая».
+4. Текст должен состоять из 5 строк.
+5. Выводимый текст должен соответствовать заданию.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Ellie is the most beautiful");
+        System.out.println("Ellie is the most beautiful");
+        System.out.println("Ellie is the most beautiful");
+        System.out.println("Ellie is the most beautiful");
+        System.out.println("Ellie is the most beautiful");
+        //напишите тут ваш код
+    }
+}
---
+++ 1.JavaSyntax/src/com/codegym/task/task01/task0142/Solution.java
@@ -0,0 +1,22 @@
+package com.codegym.task.task01.task0142;
+
+import java.util.Scanner;
+
+/*
+Write a program that displays: "Amigo is very smart".
+
+
+Требования:
+The program should output text.
+The text should begin with "Amigo".
+The text should end with "smart".
+The text should consist of 19 characters, including spaces.
+The displayed text must match the task conditions.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.print("Amigo is very smart");
+        //напишите тут ваш код
+    }
+}
