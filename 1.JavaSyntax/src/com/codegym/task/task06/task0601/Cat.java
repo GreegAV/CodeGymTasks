b48f331b4eeea24f8d62f427aa402d1be761bdce
Андрей Григорьев <greegav@gmail.com>
2019-01-11 18:00:11 +0200

    Level 6 start

---
+++ 1.JavaSyntax/src/com/codegym/task/task06/task0601/Cat.java
@@ -0,0 +1,25 @@
+package com.codegym.task.task06.task0601;
+
+/* 
+Cat's finalize method
+In the Cat class, create a protected void finalize() throws Throwable method.
+
+
+Requirements:
+1. The Cat class must have a method called finalize.
+2. The finalize method should not return anything (void).
+3. The finalize method must be protected.
+4. The finalize method can throw a Throwable exception. Indicate this in the method declaration.
+
+*/
+
+public class Cat {
+    //write your code here
+    protected void finalize() throws Throwable {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
