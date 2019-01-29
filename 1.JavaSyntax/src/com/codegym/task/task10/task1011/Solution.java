456af791d4aa1bb50dec771709e6a49ef04822a1
Андрей Григорьев <greegav@gmail.com>
2019-01-29 15:50:17 +0200

    Level 10

---
+++ 1.JavaSyntax/src/com/codegym/task/task10/task1011/Solution.java
@@ -0,0 +1,36 @@
+package com.codegym.task.task10.task1011;
+
+/* 
+Big salary
+Display "I do not want to learn Java. I want a big salary" 43 times according to the following pattern.
+
+Pattern:
+I do not want to learn Java. I want a big salary
+ do not want to learn Java. I want a big salary
+do not want to learn Java. I want a big salary
+o not want to learn Java. I want a big salary
+ not want to learn Java. I want a big salary
+not want to learn Java. I want a big salary
+…
+salary
+
+
+Requirements:
+1. The program should display text on the screen.
+2. The program should display 43 lines.
+3. The program must use a for loop or a while loop.
+4. The output should match the example in the conditions.
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "I do not want to learn Java. I want a big salary";
+        //write your code here
+        for (int i = 0; i < 43; i++) {
+            System.out.println(s.substring(i));
+        }
+    }
+
+}
+
