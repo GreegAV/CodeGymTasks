05246e23ccaa6bfabc31339a089ee2e660bb6604
Андрей Григорьев <greegav@gmail.com>
2019-02-01 16:44:57 +0200

    Level 12, 1206-1210

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1208/Solution.java
@@ -0,0 +1,36 @@
+package com.codegym.task.task12.task1208;
+
+/* 
+Freedom of the press
+Write five print methods using different parameters.
+
+
+Requirements:
+1. The program should not display text on the screen.
+2. The Solution class must have six methods.
+3. The Solution class must contain a static main() method.
+4. The Solution class must have five static print() methods with different parameters.
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+    static void print(int i) {
+    }
+
+    static void print(byte b) {
+    }
+
+    static void print(short s) {
+    }
+
+    static void print(long l) {
+    }
+
+    static void print(double d) {
+    }
+}
