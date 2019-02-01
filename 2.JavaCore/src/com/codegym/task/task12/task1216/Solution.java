964a4c20000dcc272201b863bb67230cb0a38964
Андрей Григорьев <greegav@gmail.com>
2019-02-01 17:35:22 +0200

    Level 12, 1216-1220

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1216/Solution.java
@@ -0,0 +1,29 @@
+package com.codegym.task.task12.task1216;
+
+/* 
+Wanna fly?
+Write your very own public CanFly interface.
+Add two methods to it.
+
+
+Requirements:
+1. The Solution class must have a CanFly interface.
+2. The CanFly interface must be public.
+3. The CanFly interface must have two methods.
+4. The CanFly interface must not have a parent.
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    // Add an interface here
+    public interface CanFly {
+        boolean can();
+
+        void fly();
+    }
+
+}
---
+++ 2.JavaCore/src/com/codegym/task/task12/task1218/Solution.java
@@ -0,0 +1,84 @@
+package com.codegym.task.task12.task1218;
+
+/* 
+Eat, fly, and move
+The public CanFly, CanMove, and CanEat interfaces have already been written.
+Add these interfaces to the Dog, Car, Duck, and Airplane classes.
+
+
+Requirements:
+1. The Solution class must have a CanFly interface with only one method called fly().
+2. The Solution class must have a CanMove interface with only one method called move().
+3. The Solution class must have an CanEat interface with only one method called eat().
+4. A dog must be able to move around and eat.
+5. A car must be able to move.
+6. A plane must be able to move and fly.
+7. A duck should be able to move, fly, and eat.
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanMove {
+        public void move();
+    }
+
+    public interface CanEat {
+        public void eat();
+    }
+
+    public class Dog implements CanMove, CanEat {
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
+    }
+
+    public class Duck implements CanMove, CanFly, CanEat {
+        @Override
+        public void fly() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
+    }
+
+    public class Car implements CanMove {
+        @Override
+        public void move() {
+
+        }
+    }
+
+    public class Airplane implements CanFly, CanMove {
+        @Override
+        public void fly() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
+    }
+}
