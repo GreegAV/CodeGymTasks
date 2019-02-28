dfee8961c6ea4228c9c42fd41e6975b2c16f2938
Андрей Григорьев <greegav@gmail.com>
2019-02-28 17:24:16 +0200

    Level 16, 1603

--- 2.JavaCore/src/com/codegym/task/task16/task1603/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task16/task1603/Solution.java
@@ -5,6 +5,16 @@ import java.util.List;
 
 /* 
 A list and some threads
+In the main method, add 5 threads to the static list.
+Each thread must be a new Thread object that works with its own SpecialThread object.
+
+
+Requirements:
+1. In the main method, create 5 SpecialThread objects.
+2. In the main method, create 5 Thread objects.
+3. Add 5 different threads to list.
+4. Each thread in list should work with its own SpecialThread object.
+5. The SpecialThread class's run method should display "This is the run method inside SpecialThread".
 
 */
 
@@ -13,9 +23,12 @@ public class Solution {
 
     public static void main(String[] args) {
         //write your code here
+        for (int i = 0; i < 5; i++) {
+            list.add(new Thread(new SpecialThread()));
+        }
     }
 
-    public static class SpecialThread implements Runnable {
+    public static class SpecialThread extends Thread implements Runnable {
         public void run() {
             System.out.println("This is the run method inside SpecialThread");
         }
